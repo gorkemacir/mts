4a55524ce29aef1611e1bc8147ffadf55fcecb46
Muhammed Görkem Acır <86821706+gorkemacir@users.noreply.github.com>
2023-03-17 10:46:13 +0300

    Add files via upload

---
+++ questions/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner k = new Scanner(System.in);
+        System.out.println("Matris Boyutunu Giriniz: ");
+        int n = k.nextInt();
+        // n*n lik bir matris oluşturuyoruz
+        int [][] matris = new int[n][n];
+        //bu matris için değerlerimizi giriyoruz.
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                System.out.print("[" + i + "]" + "[" + j + "]" + " için değer giriniz: ");
+                matris[i][j] = k.nextInt();
+            }
+        }
+        /*matrisin içerisindeki değerleri simetrik olup olmaması koşuluna göre dönüyoruz. Burda önemli olan iki nokta var. Birincisi
+          kontrol sağladımız değerleri tekrardan kontrol etmemek için i<j koşulunu koyduk.
+          İkinci durum isedizinin [i][j] değerlerinin eşit olduğu durumları yani istenen durumu karşılayıp karşılamadığı durumların
+           kontrolünü sağlamak */
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matris[i][j]);
+                if (i<j){
+                    if (matris[i][j] == matris[j][i]){
+                        System.out.println("simetriktir");
+                    }else System.out.println("simetrik değil");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
